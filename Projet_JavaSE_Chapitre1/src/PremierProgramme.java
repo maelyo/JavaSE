df60495f9a3a89cdfbe4b9235350243cc8152ad7
maelyo <thierrichard@gmail.com>
2019-03-03 15:49:25 +0100

    Ajout des commentaires sur la classe PremierProgramme. Ajout du fichier README.md pour présenter le projet.

--- Projet_JavaSE_Chapitre1/src/PremierProgramme.java
+++ Projet_JavaSE_Chapitre1/src/PremierProgramme.java
@@ -1,9 +1,18 @@
-
-
+/**
+ * Le nom de la classe doit correspondre au nom du fichier.
+ * @author java
+ *
+ */
 public class PremierProgramme {
 
+	/**
+	 * La méthode main est le point d'entrée d'un programme.
+	 * La signature de cette méthode ne peut pas être modifiée
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		System.out.println("Bonjour tout le monde");
+
 	}
 
 }
